642adbe4c827e6178def63b009ee5e60379918c0
Raja khan <rajak82001@gmail.com>
2024-07-13 22:48:56 +0530

    first commit

---
+++ src/pages/BuyerDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyerDashboard = () => {
+  return <div>Buyer Dashboard</div>;
+};
+
+export default BuyerDashboard;
---
+++ src/pages/SellerDashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SellerDashboard = () => {
+  return <div>Seller Dashboard</div>;
+};
+
+export default SellerDashboard;
